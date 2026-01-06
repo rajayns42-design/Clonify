98b5a6aae9661db39419cdd57466edeea3c63162
rajayns42-design <rajayns42@gmail.com>
2026-01-06 19:56:52 +0530

    Change default branch from 'master' to 'main'

--- config.py
+++ config.py
@@ -59,7 +59,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/TeamProBots/Clonify",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )
